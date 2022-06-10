243193f88bd43d35a7f4165aaf24c93ffbd08c96
Yuri S. Lima <61335084+Yuri-S-Lima@users.noreply.github.com>
2022-06-10 16:58:24 -0300

    Flexible and Expanded
    Vendo as principais diferenças entre as duas formas de organizar a disposição dos elementos na tela.

---
+++ Flexible/flexible_expanded/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flexible & Expanded',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Flexible & Expanded'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            height: 100,
+            child: Text('Item 1 - pretty big!'),
+            color: Colors.red,
+          ),
+          Flexible(
+            flex: 4,
+            fit: FlexFit.tight, // ocupa o espaço vazio
+            child: Container(
+              height: 100,
+              child: Text('Item 2'),
+              color: Colors.blue,
+            ),
+          ),
+          Flexible(
+            flex: 1, // define quem tem maior prioridade
+            fit: FlexFit.loose, // ocupa o espaço necessário
+            child: Container(
+              height: 100,
+              child: Text('Item 3'),
+              color: Colors.orange,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
