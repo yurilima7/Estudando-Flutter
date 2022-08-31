6c87a93afc4e6671705cf7aa1f1933b84e2d05e9
Yuri S. Lima <61335084+Yuri-S-Lima@users.noreply.github.com>
2022-08-31 09:44:34 -0300

    Tela de detalhes
    Inserção das listas de ingredientes da refeição e os passos para o preparo.

--- meals/lib/screens/meal_detail_screen.dart
+++ meals/lib/screens/meal_detail_screen.dart
@@ -6,6 +6,36 @@ class MealDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Widget _createSectionTitle(BuildContext context, String title){
+      return Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      );
+    }
+
+    Widget _createSectionContainer(Widget child){
+      return Container(
+
+        width: 330,
+        height: 200,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        
+        child: child,
+      );
+    }
+
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
 
     return Scaffold(
@@ -13,8 +43,69 @@ class MealDetailScreen extends StatelessWidget {
         title:  Text(meal.title),
       ),
 
-      body: const Center(
-        child: Text('Detalhes da Refeição'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // magem do prato
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+      
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+      
+            _createSectionTitle(context, 'Ingredientes'),
+            // exibição dos ingredites
+            _createSectionContainer(
+              // lista em cards os ingredientes
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: ((context, index) {
+                  
+                  return Card(
+                    color: Theme.of(context).colorScheme.secondary,
+      
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index]),
+                    ),
+                  );
+                }),
+              ),
+            ),
+      
+            _createSectionTitle(context, 'Passos'),
+            // exibição de passos
+            _createSectionContainer(
+              // listando o passo a passo do preparo
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (context, index){
+                  return Column(
+                    children: [
+
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+
+                        title: Text(meal.steps[index]),
+                      ),
+
+                      const Divider(),
+                    ],
+                  );
+                }
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
