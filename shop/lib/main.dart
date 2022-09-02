fa1890c199dc8149ca6319840b9c44d6c8d24397
Yuri Lima <yurilima742@yahoo.com>
2022-09-02 18:53:37 -0300

    Projeto Shop

---
+++ shop/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shop/screens/products_overview_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.purple,
+          secondary: Colors.deepOrange,
+        ),
+
+        fontFamily: 'Lato',
+
+      ),
+      home: ProductsOverviewPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ shop/lib/screens/products_overview_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  // recebendo a lista de produtos
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ), 
+
+        itemBuilder: (context, index) => ProductItem(product: loadedProducts[index]),
+      ),
+    );
+  }
+}
